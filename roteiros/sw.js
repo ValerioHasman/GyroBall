66f66118054b13f7678e5ce86c13bce6e0e7e7ac
Valério Hasman <valeriopro22@gmail.com>
2024-02-28 11:19:31 -0300

    serviceWorker otimizado

--- roteiros/sw.js
+++ roteiros/sw.js
@@ -1,16 +1,6 @@
 export default function sw() {
   if ("serviceWorker" in navigator) {
     navigator.serviceWorker.register('serviceWorker.js')
-      .then(function (registration) {
-        if (registration.active && navigator.onLine) {
-          registration.unregister().then(function () {
-            navigator.serviceWorker.register('serviceWorker.js')
-            .catch((eh)=>{
-              console.error('Registro Falhou', eh);
-            });
-          });
-        }
-      })
       .catch(function (error) {
         console.error('Erro ao registrar o Service Worker:', error);
       });
--- serviceWorker.js
+++ serviceWorker.js
@@ -1,6 +1,23 @@
+const CACHE_NAME = 'gb-v5';
+
+self.addEventListener('activate', function (event) {
+  event.waitUntil(
+    caches.keys().then(function (cacheNames) {
+      return Promise.all(
+        cacheNames.map(function (cacheName) {
+          if (cacheName !== CACHE_NAME) {
+            console.log("algo foi apagado");
+            return caches.delete(cacheName);
+          }
+        })
+      );
+    })
+  );
+});
+
 self.addEventListener('fetch', function (event) {
   event.respondWith(
-    caches.open('gbv1')
+    caches.open(CACHE_NAME)
       .then((cache) => {
         return cache.match(event.request).then(function (response) {
           return response || fetch(event.request).then(function (networkResponse) {
